6965f69788236f807004401a807e1f67abbc8ee2
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-01-30 22:11:57 -0300

    changes di

---
+++ Domain/Interfaces/ICustomerCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Interfaces
+{
+    public interface ICustomerCreatedEvent
+    {
+        string CustomerId { get; }
+        string Name { get; }
+        DateTime Birthday { get; }
+        string Type { get; }
+        DateTime CreatedAt { get; }
+    }
+}
