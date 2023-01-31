9a8835464a4b45c98ef59fa5fa2967d7e84ca887
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-01-30 22:09:46 -0300

    add controller

---
+++ Application.API/Controllers/Customer/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces.Customers;
+using Domain.Models.Customer;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Application.API.Controllers.Customers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] Customer customer)
+        {
+            await _customerService.CreateCustomer(customer);
+
+            return Ok();
+        }
+    }
+}
