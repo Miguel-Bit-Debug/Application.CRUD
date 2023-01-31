19229a8355d245487a9538e68c0dd4d606fe489a
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-01-30 22:10:05 -0300

    add interfaces

---
+++ Domain/Interfaces/Customer/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.Customer;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Customers
+{
+    public interface ICustomerRepository
+    {
+        Task CreateCustomer(Customer customer);
+    }
+}
---
+++ Domain/Interfaces/Customer/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.Customer;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Customers
+{
+    public interface ICustomerService
+    {
+        Task CreateCustomer(Customer customer);
+    }
+}
