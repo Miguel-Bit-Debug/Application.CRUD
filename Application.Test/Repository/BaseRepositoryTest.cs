9043da9d78dcb2e01bfb12062ffb38d58a1fa5cd
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-01-30 22:11:47 -0300

    add base test

---
+++ Application.Test/Repository/BaseRepositoryTest.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Interfaces;
+using Infra.Data.Data;
+using Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Test.Repository
+{
+    public abstract class BaseRepositoryTest
+    {
+        protected readonly IProductRepository _productRepository;
+    }
+}
---
+++ Application.Test/Repository/ProductRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Models.Product;
+using Infra.Data.Data;
+using Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Test.Repository
+{
+    public class ProductRepositoryTest
+    {
+        private readonly ProductRepository _productRepository;
+        private readonly AppDbContext _context;
+
+        public ProductRepositoryTest()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .EnableSensitiveDataLogging();
+            var options = optionsBuilder.Options;
+
+            _context = new AppDbContext(options);
+            _productRepository = new ProductRepository(_context);
+        }
+
+        [Fact]
+        public void Deve_Testar_Instancia_Repository()
+        {
+            var repository = new ProductRepository(_context);
+            Assert.True(repository != null);
+        }
+
+
+        [Fact]
+        public async Task Deve_AdicionarProduto()
+        {
+            var product = new ProductModel()
+            {
+                Id = 1,
+                Name = "teste",
+                Description = "teste"
+            };
+
+            var products = await _productRepository.GetAllProducts();
+
+            var x = 10;
+
+        }
+
+    }
+}
